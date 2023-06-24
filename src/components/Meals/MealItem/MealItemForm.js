87f28ac0f3c7c0484d1a7e0b3f73aa8344ac413e
Jan Bogosyan <janbogosyan@gmail.com>
2023-06-24 15:17:52 +0300

    Working on a More Complex Reducer Logic & Making Items Removable & code fixes

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -6,16 +6,20 @@ import classes from './MealItemForm.module.css';
 const MealItemForm = props => {
     const [amountIsValid, setAmountIsValid] = useState(true);
     const amountInputRef = useRef();
+
     const submitHandler = event => {
         event.preventDefault();
+
         const enteredAmount = amountInputRef.current.value;
         const enteredAmountNumber = +enteredAmount;
+
         if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
             setAmountIsValid(false);
             return;
         }
         props.onAddToCart(enteredAmountNumber);
     };
+
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <Input
@@ -31,7 +35,7 @@ const MealItemForm = props => {
                 }}
             />
             <button>+ Add</button>
-            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,48 +1,92 @@
-import { useReducer } from "react";    //useReducer is similar to UseState but for more complex logic
 
-import CartContext from "./cart-context";
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
 
 const defaultCartState = {
     items: [],
     totalAmount: 0,
 };
-
 //cartReducer outside of the component bcs dont need to use any data from this component & shouldnt been recreated all the time when the component is reevaluated
+//state,action are received automatic by react/ state is simply the last state snapshot of the state managed by the reducer/ action is dispatched by you later in your code
+
 const cartReducer = (state, action) => {
-    if (action.type === 'ADD_ITEM') {
-        const updatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totalAMount + action.item.price * action.item.amount;
-        return {
-            items: updatedItems,
-            totalAMount: updatedTotalAmount
-        }
+  if (action.type === 'ADD') {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
-    return defaultCartState;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
+  return defaultCartState;
 };
 
-const CartProvider = props => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-    const addItemToCarthandler = (item) => {
-        dispatchCartAction({ type: 'ADD_ITEM', item: item });
-    };
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
 
-    const removeItemFromCartHandler = (id) => {
-        dispatchCartAction({ type: 'REMOVE', id: id })
-    };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
-    const cartContext = {
-        items: cartState.items,
-        totalAmount: cartState.totalAmount,
-        addItem: addItemToCarthandler,
-        removeItem: removeItemFromCartHandler,
-    };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
 
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    );
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
