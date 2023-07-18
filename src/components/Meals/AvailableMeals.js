f4b8ff7763f94ea5704bb1773c4debd9ded797fd
Jan Bogosyan <janbogosyan@gmail.com>
2023-07-18 14:20:56 +0300

    Handling Error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,11 +8,17 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     //useEffect който по този начин записан не е async/await и не връща promis
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch('https://react-http-5ae50-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -28,13 +34,25 @@ const AvailableMeals = () => {
             setMeals(loadedMeals);
             setIsLoading(false);
         };
-        fetchMeals(); //и тука извикваме нашия fetchMeals който не е async/await ,но се намира вътре в useEffect
+
+        fetchMeals().catch((error) => { // когато искаме да catchnem error inside in promise function like fetchMeals is async
+            setIsLoading(false);
+            setHttpError(error.message);
+        }); 
     }, []);
 
     if (isLoading) {
         return (
             <section className={classes.MealsLoading}>
                 <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
             </section>
         );
     }
